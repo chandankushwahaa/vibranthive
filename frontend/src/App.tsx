cc7b85c75356f9b3be7038ba39ae5b12c77e752f
chandan <chandan7979ck@gmail.com>
2024-04-15 21:30:03 +0530

    UI blog done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import {Signup} from './pages/Signup';
 import {Signin} from './pages/Signin';
 import Home from './pages/Home';
+import { Blogs } from "./pages/Blogs";
+import Blog from "./pages/Blog";
 
 function App() {
     return (
@@ -11,6 +13,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/blog/:id" element={<Blog />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ frontend/src/pages/Blogs.tsx
@@ -0,0 +1,31 @@
+import { BlogCard } from "../components/BlogCard";
+
+export const Blogs = () => {
+
+  return <div className="flex justify-center">
+    <div className="max-w-xl">
+      <BlogCard
+        id={2}
+        authorName={ "Chandan Kushwaha"}
+        title={"CHadna  galgotias unieraactiva"}
+        content={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut repudiandae dignissimos deserunt. Corrupti explicabo, dolore ipsa, mollitia fugit tempora esse, necessitatibus aperiam itaque laudantium dolorem at excepturi nihil incidunt molestias."}
+        publishedDate={"2nd Feb 2024"}
+      />
+            <BlogCard
+        id={2}
+        authorName={ "Chandan Kushwaha"}
+        title={"CHadna  galgotias unieraactiva"}
+        content={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut repudiandae dignissimos deserunt. Corrupti explicabo, dolore ipsa, mollitia fugit tempora esse, necessitatibus aperiam itaque laudantium dolorem at excepturi nihil incidunt molestias."}
+        publishedDate={"2nd Feb 2024"}
+      />
+            <BlogCard
+        id={2}
+        authorName={ "Chandan Kushwaha"}
+        title={"CHadna  galgotias unieraactiva"}
+        content={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut repudiandae dignissimos deserunt. Corrupti explicabo, dolore ipsa, mollitia fugit tempora esse, necessitatibus aperiam itaque laudantium dolorem at excepturi nihil incidunt molestias."}
+        publishedDate={"2nd Feb 2024"}
+      />
+    </div>
+    
+  </div>
+};
